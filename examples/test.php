e06bc8c945c08117ba9c4eae4fcebf5c1667d461
DropFan <DropFan@Gmail.com>
2016-12-27 16:02:28 +0800

    add test.php

---
+++ examples/test.php
@@ -0,0 +1,46 @@
+<?PHP
+/**
+ * MIT License
+ * ===========
+ *
+ * Copyright (c) 2016 DropFan <DropFan@Gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * @package    OneSignal-server-api
+ * @author     DropFan <DropFan@Gmail.com>
+ * @copyright  2016 DropFan.
+ * @license    http://www.opensource.org/licenses/mit-license.php  MIT License
+ * @version    0.1
+ * @link       https://github.com/DropFan/onesignal-server-api
+ */
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use OneSignalApi\OneSignal;
+
+$config = require 'config.php';
+
+$api = new OneSignal($config);
+
+$apps = $api->getApps();
+
+var_dump($apps);
+
